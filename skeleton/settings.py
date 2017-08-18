427a23d10042d0f44f4fa62272a1212961d34a47
Alex D’Astous <a_dastous@wargaming.net>
2017-08-17 20:51:36 -0500

    Fix pathing bug

--- skeleton/settings.py
+++ skeleton/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'skeleton.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'skeleton.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
