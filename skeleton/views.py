5f676bdd10948cc04389f5ef6ec9272194ce572a
Alex D’Astous <a_dastous@wargaming.net>
2017-08-17 21:14:02 -0500

    Fix url regex

--- skeleton/views.py
+++ skeleton/views.py
@@ -1,5 +1,26 @@
+from random import choice
+
 from django.http import HttpResponse
 
 
 def root(request):
-    return HttpResponse("testing")
+    messages = [
+        'Yes.',
+        'No.',
+        'Maybe.',
+        'Ask again later.',
+        'Outlook not so good.',
+        'Most definitely.'
+        'Seek help.'
+        'Absolutely.'
+        'Without a doubt.'
+        'Never.'
+        'As God as my witness, this shall be.'
+        'Why would you even ask that?'
+        'Girl, please.'
+        'Indubitably'
+    ]
+
+    fortune = choice(messages)
+
+    return HttpResponse(fortune)
